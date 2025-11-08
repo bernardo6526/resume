75943dbb5962a430acab65589086ef60c0209c84
Bernardo Victor <bernardo6526@gmail.com>
2025-11-08 13:24:34 -0300

    Added navbar scroll interaction, fixed home.png

--- resume-app/app/page.tsx
+++ resume-app/app/page.tsx
@@ -1,23 +1,61 @@
+'use client';
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <Image
-        src="/backgrounds/home.png"
-        alt="Background image"
-        fill
-        unoptimized // ⛔ disables Next.js image optimization
-        style={{ objectFit: "cover" }}
-      />
+  useEffect(() => {
+    // Prevent Right Click
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('contextmenu', event => event.preventDefault());
+    }
+  });
 
-      <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-white">
-        <h1 
-          style={{ fontFamily: "var(--font-lastica)" }}
-          className="font-lastica text-4xl font-bold ">
-          Fullstack Engineer
-        </h1>
-      </div>
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center">
+      <section id="home" className="relative min-h-screen w-full bg-blue flex items-center justify-center">
+        <Image
+          src="/backgrounds/home.png"
+          alt="Background image"
+          fill
+          unoptimized // ⛔ disables Next.js image optimization
+          style={{ objectFit: "cover" }}
+        />
+        <div className="inset-0 z-10 flex flex-col items-center justify-center text-white">
+          <h1
+            style={{ fontFamily: "var(--font-lastica)" }}
+            className="font-lastica text-4xl font-bold ">
+            Fullstack Engineer
+          </h1>
+          <Image
+            src="/logo.png"
+            alt="Logo image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '20%', height: 'auto' }}
+            unoptimized // ⛔ disables Next.js image optimization
+          />
+        </div>
+      </section>
+      <section id="about" className="relative min-h-screen w-full bg-blue flex items-center justify-center">
+        <Image
+          src="/backgrounds/about.png"
+          alt="Background image"
+          fill
+          unoptimized // ⛔ disables Next.js image optimization
+          style={{ objectFit: "cover" }}
+        />
+        <div className="inset-0 z-10 flex flex-col items-center justify-center text-black">
+          <h1
+            style={{ fontFamily: "var(--font-lastica)" }}
+            className="font-lastica text-4xl font-bold">
+            About
+          </h1>
+        </div>
+      </section>
     </div>
   );
 }
--- resume-app/app/ui/nav-links.tsx
+++ resume-app/app/ui/nav-links.tsx
@@ -1,22 +1,29 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { useState, useEffect } from 'react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/'},
-  { name: 'About',href: '/about' },
-  { name: 'Experiences', href: '/professional-experiences'},
-  { name: 'Education', href: '/education'},
-  { name: 'Projects', href: '/projects'},
-  { name: 'Skills', href: '/skills'},
+  { name: 'Home', href: '#home' },
+  { name: 'About', href: '#about' },
+  { name: 'Experiences', href: '#experiences' },
+  { name: 'Education', href: '#education' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Skills', href: '#skills' },
 ];
 
+
 export default function NavLinks() {
-  const pathname = usePathname();
+  const [fragment, setFragment] = useState('');
+
+   useEffect(() => {
+    console.log("Hash:", window.location.hash);
+    setFragment(window.location.hash);
+  }, []);
+
   return (
     <>
       {links.map((link) => {
@@ -26,8 +33,9 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:text-cyan-600 md:flex-none md:justify-center md:p-2 md:px-3",
-              {'text-cyan-600': pathname === link.href,},
+              { 'text-cyan-600': fragment === link.href, },
             )}
+            onClick={() => { setFragment(link.href) }}
           >
             <p className="hidden md:block" style={{ fontFamily: "var(--font-lastica)" }}>{link.name}</p>
           </Link>
